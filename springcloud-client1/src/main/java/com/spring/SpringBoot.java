f4ea7401d2ef2663f11fbfd1f8e4cc829ec8f944
qiuyangli <15663767531@163.com>
2017-11-13 19:10:22 +0800

    One of the client
    One of the client

---
+++ springcloud-client1/src/main/java/com/spring/SpringBoot.java
@@ -0,0 +1,22 @@
+package com.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@EnableEurekaClient
+public class SpringBoot {
+	
+  @RequestMapping("/home")
+   public String home() {
+         return "Hello World,I am from client1";
+     }
+
+     public static void main(String[] args) {
+         SpringApplication.run(SpringBoot.class, args);
+     }
+}
