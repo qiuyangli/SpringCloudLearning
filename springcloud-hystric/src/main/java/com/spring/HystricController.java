31d00887d8746328be38202fdfb2524cc5f0624b
qiuyangli <15663767531@163.com>
2017-11-17 16:34:43 +0800

    Update HystricController.java

--- springcloud-hystric/src/main/java/com/spring/HystricController.java
+++ springcloud-hystric/src/main/java/com/spring/HystricController.java
@@ -5,14 +5,14 @@ import org.springframework.stereotype.Component;
 import com.spring.SpringBoot.SchedualServiceHi;
 
 public class HystricController {
-
-	@Component
-	public class SchedualServiceHiHystric implements SchedualServiceHi {
+    
+    @Component
+    public class SchedualServiceHiHystric implements SchedualServiceHi {
 	    
-		@Override
-	    public String sayHiFromClient() {
-	        return "sorry";
-		}
-	
-	}
+    @Override
+    public String sayHiFromClient() {
+    	return "sorry";
+    }	
+    
+    }
 }
