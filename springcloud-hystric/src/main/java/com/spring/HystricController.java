616cb980cf9c14173eaecab138ee021e3974c113
李秋阳 <15663767531@163.com>
2017-12-13 08:52:45 +0800

    Remove
    Remove

--- springcloud-hystric/src/main/java/com/spring/HystricController.java
+++ springcloud-hystric/src/main/java/com/spring/HystricController.java
@@ -1,18 +1,5 @@
 package com.spring;
 
-import org.springframework.stereotype.Component;
-
-import com.spring.SpringBoot.SchedualServiceHi;
-
 public class HystricController {
     
-    @Component
-    public class SchedualServiceHiHystric implements SchedualServiceHi {
-	    
-    @Override
-    public String sayHiFromClient() {
-    	return "sorry";
-    }	
-    
-    }
 }
